29804dec2952eb7bee48f8363ad75945f93cadbd
Mitsuru Kariya <kariya_mitsuru@hotmail.com>
2020-05-28 00:30:52 +0900

    Follow Github comments
    Remove unnecessary macro 'lt'.
    Change function name 'nextafter' to 'next_after'

--- wain-exec/src/cast.rs
+++ wain-exec/src/cast.rs
@@ -22,17 +22,13 @@ macro_rules! float_to_int {
             //    b) If MIN - 1.0 can not be represented exactly,
             //       then MIN - 1.0 = MIN.  (Because of "round to nearest even" behavior.)
             //       So, we can check it with MIN <= f.
-            macro_rules! lt {
-                ($min:expr, $val:expr) => {
-                    if $min != $min - 1.0 {
-                        $min - 1.0 < $val
-                    } else {
-                        $min <= $val
-                    }
-                };
-            }
+            let gt_min = if MIN != MIN - 1.0 {
+                MIN - 1.0 < f
+            } else {
+                MIN <= f
+            };
 
-            if lt!(MIN, f) && f < MAX + 1.0 {
+            if gt_min && f < MAX + 1.0 {
                 Some(f as $int)
             } else {
                 None
@@ -88,18 +84,18 @@ mod tests {
     use std::{f32, f64};
 
     trait NextAfter {
-        fn nextafter(self, _: Self) -> Self;
+        fn next_after(self, _: Self) -> Self;
     }
 
     impl NextAfter for f32 {
-        fn nextafter(self, o: f32) -> f32 {
+        fn next_after(self, o: f32) -> f32 {
             let b = self.to_bits();
             f32::from_bits(if self.abs() < o.abs() { b + 1 } else { b - 1 })
         }
     }
 
     impl NextAfter for f64 {
-        fn nextafter(self, o: f64) -> f64 {
+        fn next_after(self, o: f64) -> f64 {
             let b = self.to_bits();
             f64::from_bits(if self.abs() < o.abs() { b + 1 } else { b - 1 })
         }
@@ -108,13 +104,13 @@ mod tests {
     #[test]
     fn float_to_uint() {
         assert_eq!(f32_to_u32(-1.0), None);
-        assert_eq!(f32_to_u32(-1.0.nextafter(-0.0)), Some(0));
+        assert_eq!(f32_to_u32(-1.0.next_after(-0.0)), Some(0));
         assert_eq!(f32_to_u32(-0.0), Some(0));
         assert_eq!(f32_to_u32(0.0), Some(0));
-        assert_eq!(f32_to_u32(1.0.nextafter(0.0)), Some(0));
+        assert_eq!(f32_to_u32(1.0.next_after(0.0)), Some(0));
         assert_eq!(f32_to_u32(1.0), Some(1));
         assert_eq!(
-            f32_to_u32((u32::MAX as f32).nextafter(0.0)),
+            f32_to_u32((u32::MAX as f32).next_after(0.0)),
             Some(0xffff_ff00)
         );
         assert_eq!(f32_to_u32(u32::MAX as f32), None);
@@ -124,13 +120,13 @@ mod tests {
         assert_eq!(f32_to_u32(-f32::NAN), None);
 
         assert_eq!(f32_to_u64(-1.0), None);
-        assert_eq!(f32_to_u64(-1.0.nextafter(-0.0)), Some(0));
+        assert_eq!(f32_to_u64(-1.0.next_after(-0.0)), Some(0));
         assert_eq!(f32_to_u64(-0.0), Some(0));
         assert_eq!(f32_to_u64(0.0), Some(0));
-        assert_eq!(f32_to_u64(1.0.nextafter(0.0)), Some(0));
+        assert_eq!(f32_to_u64(1.0.next_after(0.0)), Some(0));
         assert_eq!(f32_to_u64(1.0), Some(1));
         assert_eq!(
-            f32_to_u64((u64::MAX as f32).nextafter(0.0)),
+            f32_to_u64((u64::MAX as f32).next_after(0.0)),
             Some(0xffff_ff00_0000_0000)
         );
         assert_eq!(f32_to_u64(u64::MAX as f32), None);
@@ -140,13 +136,13 @@ mod tests {
         assert_eq!(f32_to_u64(-f32::NAN), None);
 
         assert_eq!(f64_to_u32(-1.0), None);
-        assert_eq!(f64_to_u32(-1.0.nextafter(-0.0)), Some(0));
+        assert_eq!(f64_to_u32(-1.0.next_after(-0.0)), Some(0));
         assert_eq!(f64_to_u32(-0.0), Some(0));
         assert_eq!(f64_to_u32(0.0), Some(0));
-        assert_eq!(f64_to_u32(1.0.nextafter(0.0)), Some(0));
+        assert_eq!(f64_to_u32(1.0.next_after(0.0)), Some(0));
         assert_eq!(f64_to_u32(1.0), Some(1));
         assert_eq!(
-            f64_to_u32((u32::MAX as f64 + 1.0).nextafter(0.0)),
+            f64_to_u32((u32::MAX as f64 + 1.0).next_after(0.0)),
             Some(u32::MAX)
         );
         assert_eq!(f64_to_u32(u32::MAX as f64 + 1.0), None);
@@ -156,13 +152,13 @@ mod tests {
         assert_eq!(f64_to_u32(-f64::NAN), None);
 
         assert_eq!(f64_to_u64(-1.0), None);
-        assert_eq!(f64_to_u64(-1.0.nextafter(-0.0)), Some(0));
+        assert_eq!(f64_to_u64(-1.0.next_after(-0.0)), Some(0));
         assert_eq!(f64_to_u64(-0.0), Some(0));
         assert_eq!(f64_to_u64(0.0), Some(0));
-        assert_eq!(f64_to_u64(1.0.nextafter(0.0)), Some(0));
+        assert_eq!(f64_to_u64(1.0.next_after(0.0)), Some(0));
         assert_eq!(f64_to_u64(1.0), Some(1));
         assert_eq!(
-            f64_to_u64((u64::MAX as f64).nextafter(0.0)),
+            f64_to_u64((u64::MAX as f64).next_after(0.0)),
             Some(0xffff_ffff_ffff_f800)
         );
         assert_eq!(f64_to_u64(u64::MAX as f64), None);
@@ -175,18 +171,18 @@ mod tests {
     #[test]
     fn float_to_int() {
         assert_eq!(
-            f32_to_i32((i32::MIN as f32).nextafter(f32::NEG_INFINITY)),
+            f32_to_i32((i32::MIN as f32).next_after(f32::NEG_INFINITY)),
             None
         );
         assert_eq!(f32_to_i32(i32::MIN as f32), Some(i32::MIN));
         assert_eq!(f32_to_i32(-1.0), Some(-1));
-        assert_eq!(f32_to_i32(-1.0.nextafter(-0.0)), Some(0));
+        assert_eq!(f32_to_i32(-1.0.next_after(-0.0)), Some(0));
         assert_eq!(f32_to_i32(-0.0), Some(0));
         assert_eq!(f32_to_i32(0.0), Some(0));
-        assert_eq!(f32_to_i32(1.0.nextafter(0.0)), Some(0));
+        assert_eq!(f32_to_i32(1.0.next_after(0.0)), Some(0));
         assert_eq!(f32_to_i32(1.0), Some(1));
         assert_eq!(
-            f32_to_i32((i32::MAX as f32).nextafter(0.0)),
+            f32_to_i32((i32::MAX as f32).next_after(0.0)),
             Some(0x7fff_ff80)
         );
         assert_eq!(f32_to_i32(i32::MAX as f32), None);
@@ -196,18 +192,18 @@ mod tests {
         assert_eq!(f32_to_i32(-f32::NAN), None);
 
         assert_eq!(
-            f32_to_i64((i64::MIN as f32).nextafter(f32::NEG_INFINITY)),
+            f32_to_i64((i64::MIN as f32).next_after(f32::NEG_INFINITY)),
             None
         );
         assert_eq!(f32_to_i64(i64::MIN as f32), Some(i64::MIN));
         assert_eq!(f32_to_i64(-1.0), Some(-1));
-        assert_eq!(f32_to_i64(-1.0.nextafter(-0.0)), Some(0));
+        assert_eq!(f32_to_i64(-1.0.next_after(-0.0)), Some(0));
         assert_eq!(f32_to_i64(-0.0), Some(0));
         assert_eq!(f32_to_i64(0.0), Some(0));
-        assert_eq!(f32_to_i64(1.0.nextafter(0.0)), Some(0));
+        assert_eq!(f32_to_i64(1.0.next_after(0.0)), Some(0));
         assert_eq!(f32_to_i64(1.0), Some(1));
         assert_eq!(
-            f32_to_i64((i64::MAX as f32).nextafter(0.0)),
+            f32_to_i64((i64::MAX as f32).next_after(0.0)),
             Some(0x7fff_ff80_0000_0000)
         );
         assert_eq!(f32_to_i64(i64::MAX as f32), None);
@@ -218,17 +214,17 @@ mod tests {
 
         assert_eq!(f64_to_i32(i32::MIN as f64 - 1.0), None);
         assert_eq!(
-            f64_to_i32((i32::MIN as f64 - 1.0).nextafter(-0.0)),
+            f64_to_i32((i32::MIN as f64 - 1.0).next_after(-0.0)),
             Some(i32::MIN)
         );
         assert_eq!(f64_to_i32(-1.0), Some(-1));
-        assert_eq!(f64_to_i32(-1.0.nextafter(-0.0)), Some(0));
+        assert_eq!(f64_to_i32(-1.0.next_after(-0.0)), Some(0));
         assert_eq!(f64_to_i32(-0.0), Some(0));
         assert_eq!(f64_to_i32(0.0), Some(0));
-        assert_eq!(f64_to_i32(1.0.nextafter(0.0)), Some(0));
+        assert_eq!(f64_to_i32(1.0.next_after(0.0)), Some(0));
         assert_eq!(f64_to_i32(1.0), Some(1));
         assert_eq!(
-            f64_to_i32((i32::MAX as f64 + 1.0).nextafter(0.0)),
+            f64_to_i32((i32::MAX as f64 + 1.0).next_after(0.0)),
             Some(i32::MAX)
         );
         assert_eq!(f64_to_i32(i32::MAX as f64 + 1.0), None);
@@ -238,18 +234,18 @@ mod tests {
         assert_eq!(f64_to_i32(-f64::NAN), None);
 
         assert_eq!(
-            f64_to_i64((i64::MIN as f64).nextafter(f64::NEG_INFINITY)),
+            f64_to_i64((i64::MIN as f64).next_after(f64::NEG_INFINITY)),
             None
         );
         assert_eq!(f64_to_i64(i64::MIN as f64), Some(i64::MIN));
         assert_eq!(f64_to_i64(-1.0), Some(-1));
-        assert_eq!(f64_to_i64(-1.0.nextafter(-0.0)), Some(0));
+        assert_eq!(f64_to_i64(-1.0.next_after(-0.0)), Some(0));
         assert_eq!(f64_to_i64(-0.0), Some(0));
         assert_eq!(f64_to_i64(0.0), Some(0));
-        assert_eq!(f64_to_i64(1.0.nextafter(0.0)), Some(0));
+        assert_eq!(f64_to_i64(1.0.next_after(0.0)), Some(0));
         assert_eq!(f64_to_i64(1.0), Some(1));
         assert_eq!(
-            f64_to_i64((i64::MAX as f64).nextafter(0.0)),
+            f64_to_i64((i64::MAX as f64).next_after(0.0)),
             Some(0x7fff_ffff_ffff_fc00)
         );
         assert_eq!(f64_to_i64(i64::MAX as f64), None);
