0238b9b564b441b9341a967dca12faaa1e0a3508
rhysd <lin90162@yahoo.co.jp>
2020-05-30 22:36:47 +0900

    improve build script to easily build all C sources in directory

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -4,10 +4,36 @@ set -e
 
 export PATH=/usr/local/opt/llvm/bin:$PATH
 
-SRC="$1"
-BASE="${SRC%.*}"
+_build_c_file() {
+    local src base opts
+    src="$1"
+    base="${src%.*}"
+    opts=("${@:2}")
 
-set -x
+    set -x
 
-clang -O2 -nostdlib --target=wasm32 -Wl,--allow-undefined "${SRC}" -o "${BASE}.wasm"
-wasm2wat "${BASE}.wasm" > "${BASE}.wat"
+    clang -nostdlib --target=wasm32 -Wl,--allow-undefined "${src}" -o "${base}.wasm" "${opts[@]}"
+    wasm2wat "${base}.wasm" > "${base}.wat"
+
+    { set +x; } 2>/dev/null
+}
+
+_build_c_files_in_dir() {
+    local file files opts
+    files=("${1%/}"/*.c)
+    opts=("${@:2}")
+
+    for file in "${files[@]}"; do
+        _build_c_file "$file" "${opts[@]}"
+    done
+}
+
+if [ -f "$1" ]; then
+    _build_c_file "$1" "${@:2}"
+elif [ -d "$1" ]; then
+    _build_c_files_in_dir "$1" "${@:2}"
+else
+    echo "No such path: ${1}" 1>&2
+    exit 1
+fi
+
