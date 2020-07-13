20fa590236b34e16cf5256ae2b59877b07b05c67
Mitsuru Kariya <kariya_mitsuru@hotmail.com>
2020-07-13 16:06:43 +0900

    Fix label handling
    Fix:
    - At the Loop instruction, the current implementation simply 'continue's
      when repeating (ExecState::Breaking(0)), but the pop_label should be
      called to free the operand stack.
    
    Refactor:
    - The current implementation saves the result type to the Label by
      push_label, but there is no need to save the result type to the Label
      because it can be referred to by pop_label.
    - At the Loop instruction, the current implementation calls push_label
      at each iteration, but it only needs to be called once before the
      iteration begins because the content of the Label does not change at
      each iteration.
    - At the Block, Loop and If instructions, there is no need to call
      pop_label if ExecState is Continue.

--- wain-exec/src/stack.rs
+++ wain-exec/src/stack.rs
@@ -192,17 +192,16 @@ impl Stack {
         self.types.truncate(type_idx);
     }
 
-    pub fn push_label(&self, ty: Option<ValType>) -> Label {
+    pub fn push_label(&self) -> Label {
         Label {
             addr: self.top_addr(),
             type_idx: self.top_idx(),
-            has_result: ty.is_some(),
         }
     }
 
-    pub fn pop_label(&mut self, label: Label) {
+    pub fn pop_label(&mut self, label: &Label, ty: Option<ValType>) {
         // Part of 'br' instruction: https://webassembly.github.io/spec/core/exec/instructions.html#exec-br
-        if label.has_result {
+        if ty.is_some() {
             let v: Value = self.pop();
             self.restore(label.addr, label.type_idx);
             self.push(v);
@@ -278,7 +277,6 @@ impl<'f> CallFrame<'f> {
 pub struct Label {
     addr: usize,
     type_idx: usize,
-    has_result: bool,
 }
 
 #[cfg(test)]
